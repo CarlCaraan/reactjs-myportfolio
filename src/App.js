3045cc4f2ebfa40e95a579741a169948701e611a
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-03-24 10:27:31 +0800

    3/24/22

--- src/App.js
+++ src/App.js
@@ -167,7 +167,7 @@ export default function App() {
           </div>
         </div>
         <h1 className="text-center main-caption" data-aos="fade">
-          ~ Let's Build Something Epic! :)
+          &lt; Let's Build Something Epic /&gt;
         </h1>
       </section>
 
