927d91760aaa8b5252d72c1d70ddd12899b834ad
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-04-08 17:16:16 +0800

    4/8/22

--- src/components/UI/LoadingSpinner.js
+++ src/components/UI/LoadingSpinner.js
@@ -3,10 +3,10 @@ import classes from "./LoadingSpinner.module.css";
 const LoadingSpinner = () => {
   return (
     <div className={`${classes["centered"]}`}>
-      <div class={`${classes["loader-container"]}`}>
-        <div class={`${classes["loader"]}`}>
-          <div class={`${classes["square"]} ${classes["one"]}`}></div>
-          <div class={`${classes["square"]} ${classes["two"]}`}></div>
+      <div className={`${classes["loader-container"]}`}>
+        <div className={`${classes["loader"]}`}>
+          <div className={`${classes["square"]} ${classes["one"]}`}></div>
+          <div className={`${classes["square"]} ${classes["two"]}`}></div>
         </div>
       </div>
     </div>
